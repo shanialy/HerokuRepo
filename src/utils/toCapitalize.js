d6c00b3b29b6752f6ed002771440ad52eb4cf5c4
Shani <97959691+shanialy@users.noreply.github.com>
2023-02-25 02:36:15 +0500

    Update toCapitalize.js

--- src/utils/toCapitalize.js
+++ src/utils/toCapitalize.js
@@ -1,11 +1,4 @@
 const toCapitalize = (word) => {
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
-const toCapitalize2 = (word) => {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-}
-const toCapitalize3 = (word) => {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-}
-
-export {toCapitalize,toCapitalize2};
+export {toCapitalize};
